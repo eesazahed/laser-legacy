db7cd91417c4e170d953d7525019227e6281320f
Eesa Zahed <eszhd1@gmail.com>
2022-09-01 15:01:49 -0400

    welcome page editing...again

--- pages/auth/welcome.tsx
+++ pages/auth/welcome.tsx
@@ -1,45 +1,25 @@
 import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
 import PageHead from "../../components/PageHead";
-import SignedOut from "../../components/SignedOut";
 import styles from "../../styles/Welcome.module.css";
 
 const Welcome: NextPage = () => {
   const { data: session, status } = useSession();
 
-  const router = useRouter();
-
-  useEffect(() => {
-    router.push("/settings");
-  });
-
   return (
     <div className={styles.container}>
       <PageHead title="Welcome" />
+      <main className={styles.main}>
+        <h1 className={styles.title}>Welcome</h1>
 
-      {status === "loading" && (
-        <main className={styles.main}>
-          <p className={styles.description}>Loading...</p>
-        </main>
-      )}
-
-      {status === "unauthenticated" && <SignedOut />}
-
-      {status === "authenticated" && (
-        <main className={styles.main}>
-          <h1 className={styles.title}>Welcome</h1>
-
-          <p className={styles.description}>
-            Thanks for signing up!{" "}
-            <Link href="/settings">
-              <a className={styles.link}>Let&apos;s go! &rarr;</a>
-            </Link>
-          </p>
-        </main>
-      )}
+        <p className={styles.description}>
+          Thanks for signing up!{" "}
+          <Link href="/settings">
+            <a className={styles.link}>Let&apos;s go! &rarr;</a>
+          </Link>
+        </p>
+      </main>
     </div>
   );
 };
