8700981c160ad08dcdf10d83e0c6db8b93cfefaa
Eesa Zahed <eszhd1@gmail.com>
2022-08-31 20:33:47 -0400

    add 404 and 500 pages

---
+++ pages/404.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const Custom404: NextPage = () => {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>404 - Page Not Found</h1>
+    </main>
+  );
+};
+
+export default Custom404;
---
+++ pages/500.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const Custom500: NextPage = () => {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>500 - Page Not Found</h1>
+    </main>
+  );
+};
+
+export default Custom500;
