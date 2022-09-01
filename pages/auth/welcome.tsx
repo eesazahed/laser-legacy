4db5b37382465b05911084fc1d5250777311a989
Eesa Zahed <eszhd1@gmail.com>
2022-09-01 14:49:14 -0400

    welcome page on sign in stuff

--- pages/auth/welcome.tsx
+++ pages/auth/welcome.tsx
@@ -1,12 +1,20 @@
 import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import PageHead from "../../components/PageHead";
 import SignedOut from "../../components/SignedOut";
 import styles from "../../styles/Welcome.module.css";
 
 const Welcome: NextPage = () => {
   const { data: session, status } = useSession();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/settings");
+  });
 
   return (
     <div className={styles.container}>
@@ -26,7 +34,7 @@ const Welcome: NextPage = () => {
 
           <p className={styles.description}>
             Thanks for signing up!{" "}
-            <Link href="/">
+            <Link href="/settings">
               <a className={styles.link}>Let&apos;s go! &rarr;</a>
             </Link>
           </p>
