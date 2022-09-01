e7cf6401b184b03e5c71052f016cc3503fef05d4
Eesa Zahed <eszhd1@gmail.com>
2022-08-31 20:34:32 -0400

    server side error handling client side

--- pages/500.tsx
+++ pages/500.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 const Custom500: NextPage = () => {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>500 - Page Not Found</h1>
+      <h1 className={styles.title}>500 - Server-side error occurred</h1>
     </main>
   );
 };
