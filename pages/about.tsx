7fa26a2ef15d92293a00595bcbcaf2e573af15ee
Eesa Zahed <eesa.zahed@gmail.com>
2024-03-15 16:33:18 -0400

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -13,11 +13,6 @@ const About: NextPage = () => {
         This platform is supposed to be a place where people can enjoy other
         users&apos; posts without tracking or ads. This site is free for anyone,
         anywhere to use without distraction.
-        <br />
-        <br />
-        Sign up now if your ready! If not, then don&apos;t worry. Humans have
-        survived millions of years without the need for social media, so
-        I&apos;m sure you&apos;ll be fine if you&apos;d prefer not to.
       </p>
 
       <div className={styles.grid}>
